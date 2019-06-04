6d6883d42cb1bf9545c1aa06540bd2ec283a45e5
Grace Tan <gracetan37@gmail.com>
2019-06-04 16:41:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,9 @@
 # Add  code here!
 
 def prime?(x)
-  if x == 1
-    return true 
-  elsif x < 1
+  if x < 2
     return false
-  else
+  elsif
   for y in 2..(x-1)
     if (x % y) == 0
       return false 

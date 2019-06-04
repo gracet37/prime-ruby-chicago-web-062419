bae278cab5affc714726df03e6f9ce6b2aa00f9a
Grace Tan <gracetan37@gmail.com>
2019-06-04 16:32:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,11 @@
 
 def prime?(n)
 false if n < 2
-
+  (2...num).each do |factor|
+    if (num % factor).zero? 
+      return false
+    end
   end
+  
+  true # after checking all numbers and not receiving false
 end

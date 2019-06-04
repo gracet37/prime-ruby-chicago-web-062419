91b6315f6d07d2f83ac837d1b3e44a6acb7e8d63
Grace Tan <gracetan37@gmail.com>
2019-06-04 16:30:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,8 @@
 # Add  code here!
 
 def prime?(n)
-  d = 2..(n - 1)
-   if "#{n} % #{d}" != 0
-    return true
- elsif "#{n} % #{d}" == 0
-   return false
- elsif n < 0
- return false
+   if n < 0 
+     false
+
   end
 end
